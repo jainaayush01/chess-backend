1491f09f48c4b6c2d2e75ae90aca2aaaadb81cb2
Aayush Jain <imjainaayush@gmail.com>
2021-11-11 19:34:21 +0530

    feat: videochat and playerLeft Info

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ var gameIO
 var gameClientSocket
 var gamesInSession = []
 var gameInfo = {}
+var playerToGame = {}
 
 
 const game = (io, clientSocket) => {
@@ -14,12 +15,28 @@ const game = (io, clientSocket) => {
 	gameClientSocket.on('sendMessage', onSendMessage);
 	gameClientSocket.on('startGame', onStartGame);
 
+  videoChatBackend();
 }
 
+function videoChatBackend() {
+  gameClientSocket.on("callUser", (data) => {
+    gameIO.to(data.userToCall).emit('hey', { signal: data.signalData, from: data.from });
+    return;
+  })
 
-function onDisconnect(gameId) {
-	var i = gamesInSession.indexOf(gameId);
-	gamesInSession.splice(i, 1);
+  gameClientSocket.on("acceptCall", (data) => {
+    gameIO.to(data.to).emit('callAccepted', data.signal);
+    return;
+  })
+}
+
+function onDisconnect() {
+  let gameId = playerToGame[this.id];
+  if(gameId) {
+    var i = gamesInSession.indexOf(gameId);
+    gameIO.to(gameId).emit('playerLeft', gameId);
+    gamesInSession.splice(i, 1);
+  }
 }
 
 /**
@@ -36,6 +53,7 @@ function onSendMessage(message) {
 
 function onCreateGame({ username, gameId }) {
 	var color = 1; gamesInSession.push(gameId);
+  playerToGame[this.id] = gameId;
 	gameInfo[gameId] = {
 		gameId: gameId,
 		p1Username: username,
@@ -50,6 +68,7 @@ function onCreateGame({ username, gameId }) {
 }
 
 function onStartGame({ username, gameId }) {
+  playerToGame[this.id] = gameId;
 	var color = 0; var room = gameIO.sockets.adapter.rooms.has(gameId);
 	if (!room) {
 		this.emit('statusUpdate', 'This game session dne.');
